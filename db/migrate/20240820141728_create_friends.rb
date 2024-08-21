77e0243c23992b78c9a112032875a772bd5d8e0a
giffordjoe <jgifford@revela.co>
2024-08-21 10:50:43 -0400

    adding null constraint to friends schema

--- db/migrate/20240820141728_create_friends.rb
+++ db/migrate/20240820141728_create_friends.rb
@@ -1,9 +1,9 @@
 class CreateFriends < ActiveRecord::Migration[7.0]
   def change
     create_table :friends do |t|
-      t.string :first_name
-      t.string :last_name
-      t.date :birth_date
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.date :birth_date, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,9 @@
 
 ActiveRecord::Schema[7.0].define(version: 2024_08_20_141750) do
   create_table "friends", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.date "birth_date"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.date "birth_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
